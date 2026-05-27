dd3b31d4e9dade5b8f835dc20234a443ca02778f
ArsenMal <hasherimago@gmail.com>
2026-05-27 11:09:58 +0200

    el('img' change width and heigh

--- app/api/og/route.js
+++ app/api/og/route.js
@@ -63,8 +63,8 @@ export async function GET(request) {
         el('img', {
           position: 'absolute',
           top: 0, left: 0, right: 0, bottom: 0,
-          width: '100%',
-          height: '100%',
+          width: 900,
+          height: 472,
           objectFit: 'cover',
           opacity: 0.5,
         }, undefined, {
